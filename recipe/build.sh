b67e62c610e6677ca5cd8a511df7e2e1f44d7743
Nick <nicholas.curtis@uconn.edu>
2018-04-24 16:57:31 -0400

    remove duplicate

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-./configure --prefix=${PREFIX} --with-blas=openblas --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+./configure --prefix=${PREFIX} --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
 make -j ${CPU_COUNT}
 make check -j ${CPU_COUNT}
 make install

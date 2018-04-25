ddc2a585de3a8f861ff3254602f0a8d2956da173
Nick <nicholas.curtis@uconn.edu>
2018-04-25 15:51:20 -0400

    attempt to fix #2 by explicitly specifying arch / tune

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-./configure --prefix=${PREFIX} --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+./configure --prefix=${PREFIX} --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" CXXFLAGS='-march=x86-64 -mtune=generic -g -O2 -fopenmp'
 make -j ${CPU_COUNT}
 make check -j ${CPU_COUNT}
 make install

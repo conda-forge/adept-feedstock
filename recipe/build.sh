d25bdeeebbb8c4bd2f2463dd5223a9cd2c2a07bf
Nick <nicholas.curtis@uconn.edu>
2018-06-15 09:31:55 -0400

    try defining packetsize as 1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-./configure --prefix=${PREFIX} --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" CXXFLAGS='-mno-sse2 -mno-avx -g -O2 -fopenmp'
+./configure --prefix=${PREFIX} --with-blas=openblas LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib" CXXFLAGS='-DADEPT_DOUBLE_PACKET_SIZE=1 -DADEPT_FLOAT_PACKET_SIZE=1 -g -O2 -fopenmp'
 make -j ${CPU_COUNT}
 make check
 make install
